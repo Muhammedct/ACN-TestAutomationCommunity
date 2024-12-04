47d53e452cc4f5c57c6dddf84f412bfa45dd6ff4
Muhammedct <m.topcu9696@gmail.com>
2024-12-04 13:39:40 +0300

    order.txt added run.bat updated

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 cd C:\Users\muhammed.topcu\PycharmProjects\TestRobot-contactList
-pabot --processes 2 --outputdir Results tests\*.robot
+pabot --processes 2 --outputdir Results --ordering order.txt tests\*.robot
